50532524af0059ebd6ff7b42b42ad2d1c189fd6a
Th3F4nd1t <indephisive@gmail.com>
2025-05-14 08:00:11 -0400

    removed unused imports

--- 2025/Dragonfly/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ 2025/Dragonfly/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,12 +4,9 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public class Drivetrain extends SubsystemBase {
